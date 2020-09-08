d45833dcd56f64d90646fd3b429f9fc4ed5ec9c4
George Gardner <gpgardner@yahoo.com>
2020-09-08 17:28:41 -0400

    adding context api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
