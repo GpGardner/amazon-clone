faf037261017bd3c5fa2ceea2484438af02b8b78
George Gardner <gpgardner@yahoo.com>
2020-09-08 15:11:15 -0400

    adding in firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6oD8xe1iUVROgcsav0ec20kAunZijW-U",
+  authDomain: "clone-ae657.firebaseapp.com",
+  databaseURL: "https://clone-ae657.firebaseio.com",
+  projectId: "clone-ae657",
+  storageBucket: "clone-ae657.appspot.com",
+  messagingSenderId: "128539640838",
+  appId: "1:128539640838:web:b28cdec13236bf72c76f87",
+  measurementId: "G-4EHCE1WXEX",
+};
