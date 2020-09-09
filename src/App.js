904e94d7d56b84c883a8a1fc6c9f270d4cea7e09
George Gardner <gpgardner@yahoo.com>
2020-09-09 16:24:22 -0400

    Adding login path

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import Checkout from "./Checkout"
+import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
