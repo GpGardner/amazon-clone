0764de0c1feb3c25e4f10e0e1cb2c770c38036a1
George Gardner <gpgardner@yahoo.com>
2020-09-09 17:16:04 -0400

    adding users email to checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ user, basket }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -15,6 +15,7 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._cb423492668_.jpg"
           alt=""
         />
+        <h3>Hello, {user?.email}</h3>
         <h2 className="checkout__title">Your Shopping Basket</h2>
 
         {basket.map((item) => (
