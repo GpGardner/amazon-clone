3449d58db760451d3408f6b991d0221a6c75c3cd
George Gardner <gpgardner@yahoo.com>
2020-09-10 15:01:25 -0400

    Logout doesn't send to register page

--- src/Header.js
+++ src/Header.js
@@ -33,7 +33,7 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to="/login" className="header__signIn">
+        <Link to={!user && "/login"} className="header__signIn">
           <div onClick={handleAuthentication} className="header__option">
             <span className="header__optionLineOne">Hello {user ? user.email : 'Guest'}</span>
             <span className="header__optionLineTwo">{user ? 'Sign Out?' : "Sign In"}</span>
