5c7244f423782579e6d211ac25c57462eb6a7ad3
George Gardner <gpgardner@yahoo.com>
2020-09-10 20:33:40 -0400

    created checkout product

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,46 +1,43 @@
-import React from 'react'
-import './CheckoutProduct.css'
-import { useStateValue } from './StateProvider';
+import React from "react";
+import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 //Material-UI
-import StarIcon from '@material-ui/icons/Star';
+import StarIcon from "@material-ui/icons/Star";
 
-
-
-function CheckoutProduct({id, image, title, price, rating}) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
 
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
 
-	const removeFromBasket = () => {
-		dispatch({
-			type: 'REMOVE_FROM_BASKET',
-			id: id
-		})
-	}
+  return (
+    <div className="checkoutProduct">
+      <img src={image} className="checkoutProduct__image" />
 
-
-
-	return (
-		<div className="checkoutProduct">
-				<img src={image} className="checkoutProduct__image"/>
-
-				<div className="checkoutProduct__info">
-					<p className="checkoutProduct__title">{title}</p>
-					<p className="checkoutProduct__price">
-						<small>$</small>
-						<strong>{price}</strong>
-						</p>
-					<div className="checkoutProduct__rating">
-						{Array(rating)
-							.fill()
-							.map((_, i) => (
-								<StarIcon key={i}/>
-							))}
-					</div>
-					<button onClick={removeFromBasket}>Remove from basket</button>
-				</div>
-		</div>
-	)
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon key={i} />
+            ))}
+        </div>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from basket</button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutProduct
+export default CheckoutProduct;
