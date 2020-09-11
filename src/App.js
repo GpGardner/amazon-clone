5dce762819677cfaf06069e0ec0943a4f2849e11
George Gardner <gpgardner@yahoo.com>
2020-09-10 20:32:40 -0400

    adding orders route

--- src/App.js
+++ src/App.js
@@ -4,32 +4,38 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Orders from "./Orders";
+import Payment from "./Payment";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HPvukHhd2uwz0XxgvH5bisgyzO4Q7nF6zHPwGEgPtzt9oF8BMpAi6wxIt0GBUNEYsmWhSX627aQMCdO0JgHO8zH00MVQgLBDw"
+);
 
 function App() {
-
   const [{}, dispatch] = useStateValue();
-  
 
   useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
-      console.log('USER', authUser);
+    auth.onAuthStateChanged((authUser) => {
+      console.log("USER", authUser);
 
-      if(authUser) {
+      if (authUser) {
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <Router>
@@ -38,6 +44,16 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/login">
             <Login />
